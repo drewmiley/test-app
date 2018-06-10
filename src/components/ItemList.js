f16b3b3d59533df435e02ff63f03fc2414d81857
Drew Miley <drewmiley92@gmail.com>
2018-06-10 23:17:38 +0100

    Changes to https

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,7 +4,7 @@ import { itemsFetchData } from '../actions/items';
 
 class ItemList extends Component {
     componentDidMount() {
-        this.props.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+        this.props.fetchData('https://5826ed963900d612000138bd.mockapi.io/items');
     }
 
     render() {
