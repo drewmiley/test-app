b3b7b8c48c2ac77306a0ae000db963c795bcbf1e
Drew Miley <drewmiley92@gmail.com>
2018-06-10 23:13:38 +0100

    Adds redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(); // You can also pass in an initialState here
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
