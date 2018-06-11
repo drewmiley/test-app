d70bea6687c3ea3fa02e5ccd2bb440b5d9279963
Drew Miley <drew.miley@opencastsoftware.com>
2018-06-11 13:17:45 +0100

    Moves App to containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import ItemList from './components/ItemList';
+import './App.css';
+import ItemList from '../components/ItemList';
 
 class App extends Component {
   render() {
